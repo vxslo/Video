e506220816aeafb38b7100c8a619d0450136bc28
kolasi Cochrane <46907574+vxslo@users.noreply.github.com>
2022-06-30 19:17:26 +0000

    ALot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("RatatouilleEnding","RatatouilleEndingVideo.mp4",.1,.1,10)
+    encoder.encode("INTRO","VIDEO.m4v",1,1,10)
 
 if __name__== "__main__":
     main()
